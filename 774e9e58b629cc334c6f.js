f3bb13d3609e80a5888374af8d02e6a1e20730d9
Nick <36767444+njcos@users.noreply.github.com>
2025-02-22 14:25:55 -0500

    again

---
+++ 774e9e58b629cc334c6f.js
@@ -0,0 +1,8 @@
+import "./styles.css"
+import { sidebar } from "../components/sidebar"
+import { AddItem } from "../components/addItem"
+import { load } from "../components/localLoad"
+
+sidebar()
+AddItem()
+load()
